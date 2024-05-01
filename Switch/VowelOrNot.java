befb8a3fe56b7d63a650c3e57870823f3b57c917
Shijo <130920346+shijo9345@users.noreply.github.com>
2024-05-01 20:10:00 +0530

    Add files via upload

---
+++ Switch/VowelOrNot.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class VowelOrNot {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter any Character:");
+        char ch=scanner.next().charAt(0);
+        switch (ch){
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Not a Vowel");
+    }
+    }
+}
